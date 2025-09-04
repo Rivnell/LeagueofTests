c71c11040679f183001f8c217ceaecd5920dc9e7
Ela <107350568+Rivnell@users.noreply.github.com>
2025-09-04 12:55:57 +0200

    Update POManager.js

--- Lambdatest/POManager.js
+++ Lambdatest/POManager.js
@@ -1,5 +1,6 @@
 const {HomePage} = require('./HomePage');
 const {LoginPage} = require('./LoginPage');
+const {Checkout} = require('./Checkout');
 const {Utils}= require('./Utils');
 const {cfgE2E} = require('./E2Econfigs/cfgE2E');
 
@@ -8,6 +9,7 @@ constructor(page) {
 this.page = page;
 this.homePage = new HomePage(page);
 this.loginPage = new LoginPage(page);
+this.checkout = new Checkout(page);
 this.utils = new Utils(page); 
 this.cfgE2E = new cfgE2E();
 }
@@ -15,6 +17,7 @@ this.cfgE2E = new cfgE2E();
 getHomePage() {return this.homePage;}
 getLoginPage() {return this.loginPage;}
 getUtils() {return this.utils;}
+getCheckoutPage() {return this.checkout;}
    
 }
 module.exports = {POManager};
