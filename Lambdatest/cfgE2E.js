efd2db4ebbbf53e01ecdc332346756a3af39e882
Ela <107350568+Rivnell@users.noreply.github.com>
2025-09-04 12:55:08 +0200

    Update cfgE2E.js

--- Lambdatest/cfgE2E.js
+++ Lambdatest/cfgE2E.js
@@ -4,8 +4,13 @@ class cfgE2E {
        // this.utils = new Utils();
         this.email = "teemoscout1@abc.com";
         this.password = "G7k@z!pQ";
-     
+        this.firstName = "Teemo";
+        this.lastName = "Scout";
+        this.address1 = "Forest Lane 2";
+        this.city = "Bandle City";
+        this.postcode = "12345";
+        this.country = "United Kingdom";
+        this.region = "Glasgow";
     }
-
 }
 module.exports = { cfgE2E };
